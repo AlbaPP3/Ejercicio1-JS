de6236a1392ef75c87f1b0f85c34456129c21c90
AlbaPP3 <95907455+AlbaPP3@users.noreply.github.com>
2023-02-02 14:06:04 +0100

    Initial commit

---
+++ Ejercicio1.js
@@ -0,0 +1,103 @@
+'Use strict';
+//Bucle para recorrer los programadores y sus tareas
+for(let i = 0;i < programadores.length; i++){
+  const tareas = programadores[i].tareas;
+}
+//Array para guardar las tareas de cada programador que antes hemos recorrido 
+programadoresTareas.push(tareas);
+//Bucle para recorrer este array de tareas que hemos guardado
+for(let t = 0; t < tareas.length; t++){
+  //Array para almacenar el tiempo de desarollo de cada programador
+  tiempoDesarrollo.push(tareas[t].tiempoDesarrollo);
+  tiempoMaximo = tiempoMaximo(tiempoDesarrollo);
+
+  if(tiempoMaximo == programadores[i].tareas[t].tiempoDesarrollo){
+    TareaMayorTiempo = programadores[i].tareas[t].idTarea;
+  }
+}
+let programadores = [
+  {
+    nombre: 'Juan',
+    id: 1,
+    tareas: [
+      {
+        nombreTarea: 'Tarea 1.1',
+        idTarea: 1.1,
+        tiempoDesarrollo: '25 horas',
+      },
+      {
+        nombreTarea: 'Tarea 1.2',
+        idTarea: 1.2,
+        tiempoDesarrollo: '10 horas',
+      },
+      { nombreTarea: 'Tarea 1.3', idTarea: 1.3, tiempoDesarrollo: '2 horas' },
+      {
+        nombreTarea: 'Tarea 1.4',
+        idTarea: 1.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'Lucia',
+    id: 2,
+    tareas: [
+      { nombreTarea: 'Tarea 2.1', idTarea: 2.1, tiempoDesarrollo: '5 horas' },
+      { nombreTarea: 'Tarea 2.2', idTarea: 2.2, tiempoDesarrollo: '1 hora' },
+      {
+        nombreTarea: 'Tarea 2.3',
+        idTarea: 2.3,
+        tiempoDesarrollo: '26 horas',
+      },
+      {
+        nombreTarea: 'Tarea 2.4',
+        idTarea: 2.4,
+        tiempoDesarrollo: '3.75 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'David',
+    id: 3,
+    tareas: [
+      { nombreTarea: 'Tarea 3.1', idTarea: 3.1, tiempoDesarrollo: '7 horas' },
+      {
+        nombreTarea: 'Tarea 3.2',
+        idTarea: 3.2,
+        tiempoDesarrollo: '6 horas',
+      },
+      {
+        nombreTarea: 'Tarea 3.3',
+        idTarea: 3.3,
+        tiempoDesarrollo: '9 horas',
+      },
+      {
+        nombreTarea: 'Tarea 3.4',
+        idTarea: 3.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+  {
+    nombre: 'Manuel',
+    id: 4,
+    tareas: [
+      { nombreTarea: 'Tarea 4.1', idTarea: 4.1, tiempoDesarrollo: '7 horas' },
+      {
+        nombreTarea: 'Tarea 4.2',
+        idTarea: 4.2,
+        tiempoDesarrollo: '16 horas',
+      },
+      {
+        nombreTarea: 'Tarea 4.3',
+        idTarea: 4.3,
+        tiempoDesarrollo: '29 horas',
+      },
+      {
+        nombreTarea: 'Tarea 4.4',
+        idTarea: 4.4,
+        tiempoDesarrollo: '2.5 horas',
+      },
+    ],
+  },
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio1-JS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ejercicios-bl-2-1-t4zdpk)
